2e2757beb025186bf6351ecdcb0fdbcd48c149f1
Hana Stamberkova <hanastamberkova@seznam.cz>
2025-03-11 19:11:05 +0100

    Přidání řešení prvního úkolu z cyklu cvičení.

---
+++ 01/3Cviceni_01_Tombola.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+#def losovani():
+#    vyherci = []
+#    for tah in range(3):
+#        vyherce = randint(1,1001)
+#        vyherci.append(vyherce)
+#    return vyherci
+
+# print(losovani())
+
+for tah in range(3):
+    print(randint(1,1001))
